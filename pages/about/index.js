c765e5e41c89edea8d05586f79b971da6d0038b4
Kanghan <kanghan123456789@gmail.com>
2023-03-11 21:20:18 +0700

    first commit

---
+++ pages/about/index.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div>
+        <h3>This is About Page</h3>
+        <a href="/port">To Port</a>
+      </div>
+    )
+  }
---
+++ pages/about/snoopydoo.js
@@ -0,0 +1,8 @@
+export default function Snoopy() {
+    return (
+      <div>
+        <h3>My name is Scoopydoo pydoo where r u??
+        </h3>
+      </div>
+    )
+  }
